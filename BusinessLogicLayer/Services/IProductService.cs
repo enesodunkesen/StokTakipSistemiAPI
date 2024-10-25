82aa9df2a040c9b22a41a38a8ce1df3f8c52830b
tugceozkok <152068309+tugceozkok@users.noreply.github.com>
2024-10-25 05:01:38 -0700

    Delete BusinessLogicLayer directory

--- BusinessLogicLayer/Services/IProductService.cs
+++
@@ -1,8 +0,0 @@
-public interface IProductService
-{
-	Task<IEnumerable<Product>> GetAllProductsAsync();
-	Task<Product> GetProductByIdAsync(int id);
-	Task AddProductAsync(Product product);
-	Task UpdateProductAsync(Product product);
-	Task DeleteProductAsync(int id);
-}
