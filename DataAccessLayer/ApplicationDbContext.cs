df5b3e883d44a1aacf159e0d27b6a18f9c3c17ea
Enes Odunkesen <enesodunkesen@hotmail.com>
2024-10-25 17:22:24 +0300

    DbContext yenilendi

---
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace StockManagementSystem.DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<StockMovement> StockMovements { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Transfer> Transfers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Stock>()
+                .HasOne(s => s.Product)
+                .WithMany()
+                .HasForeignKey(s => s.ProductId);
+
+            modelBuilder.Entity<Stock>()
+                .HasOne(s => s.Warehouse)
+                .WithMany(w => w.Stocks)
+                .HasForeignKey(s => s.WarehouseId);
+
+            modelBuilder.Entity<StockMovement>()
+                .HasOne(sm => sm.Product)
+                .WithMany()
+                .HasForeignKey(sm => sm.ProductId);
+
+            modelBuilder.Entity<StockMovement>()
+                .HasOne(sm => sm.Warehouse)
+                .WithMany()
+                .HasForeignKey(sm => sm.WarehouseId);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Product)
+                .WithMany()
+                .HasForeignKey(s => s.ProductId);
+
+            modelBuilder.Entity<Transfer>()
+                .HasOne(t => t.Product)
+                .WithMany()
+                .HasForeignKey(t => t.ProductId);
+
+            modelBuilder.Entity<Transfer>()
+                .HasOne(t => t.FromWarehouse)
+                .WithMany()
+                .HasForeignKey(t => t.FromWarehouseId);
+
+            modelBuilder.Entity<Transfer>()
+                .HasOne(t => t.ToWarehouse)
+                .WithMany()
+                .HasForeignKey(t => t.ToWarehouseId);
+        }
+    }
+}
