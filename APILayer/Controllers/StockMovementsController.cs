e61cdc6370c840864b395794918ed75a632f4ff6
Enes Odunkesen <enesodunkesen@hotmail.com>
2024-11-02 12:59:23 +0300

    Controllerlar eklendi

---
+++ APILayer/Controllers/StockMovementsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManagementSystem.DataAccessLayer;
+
+namespace StokTakipSistemiAPI.APILayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockMovementsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockMovementsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StockMovements
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StockMovement>>> GetStockMovements()
+        {
+            return await _context.StockMovements.ToListAsync();
+        }
+
+        // GET: api/StockMovements/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StockMovement>> GetStockMovement(int id)
+        {
+            var stockMovement = await _context.StockMovements.FindAsync(id);
+
+            if (stockMovement == null)
+            {
+                return NotFound();
+            }
+
+            return stockMovement;
+        }
+
+        // PUT: api/StockMovements/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStockMovement(int id, StockMovement stockMovement)
+        {
+            if (id != stockMovement.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stockMovement).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockMovementExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/StockMovements
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<StockMovement>> PostStockMovement(StockMovement stockMovement)
+        {
+            _context.StockMovements.Add(stockMovement);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStockMovement", new { id = stockMovement.Id }, stockMovement);
+        }
+
+        // DELETE: api/StockMovements/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStockMovement(int id)
+        {
+            var stockMovement = await _context.StockMovements.FindAsync(id);
+            if (stockMovement == null)
+            {
+                return NotFound();
+            }
+
+            _context.StockMovements.Remove(stockMovement);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StockMovementExists(int id)
+        {
+            return _context.StockMovements.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ APILayer/Controllers/TransfersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManagementSystem.DataAccessLayer;
+
+namespace StokTakipSistemiAPI.APILayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransfersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransfersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Transfers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transfer>>> GetTransfers()
+        {
+            return await _context.Transfers.ToListAsync();
+        }
+
+        // GET: api/Transfers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transfer>> GetTransfer(int id)
+        {
+            var transfer = await _context.Transfers.FindAsync(id);
+
+            if (transfer == null)
+            {
+                return NotFound();
+            }
+
+            return transfer;
+        }
+
+        // PUT: api/Transfers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTransfer(int id, Transfer transfer)
+        {
+            if (id != transfer.TransferId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(transfer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransferExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Transfers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Transfer>> PostTransfer(Transfer transfer)
+        {
+            _context.Transfers.Add(transfer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTransfer", new { id = transfer.TransferId }, transfer);
+        }
+
+        // DELETE: api/Transfers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTransfer(int id)
+        {
+            var transfer = await _context.Transfers.FindAsync(id);
+            if (transfer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transfers.Remove(transfer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TransferExists(int id)
+        {
+            return _context.Transfers.Any(e => e.TransferId == id);
+        }
+    }
+}
